cc8e1ee9173d174218f5aa40f004ce0bde336ad5
guangshuqi <guangshuqi@gmail.com>
2023-04-09 18:35:25 -0400

    set env variable to ec2

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+# Set environment variables on the EC2 instance
+echo "export ENVIRONMENT=production" >> ~/.bashrc
+echo "export OPENAI_API_KEY=${OPENAI_API_KEY}" >> ~/.bashrc
+echo "export DISCORD_BOT_TOKEN=${DISCORD_BOT_TOKEN}" >> ~/.bashrc
+
+# Reload the .bashrc file
+source ~/.bashrc
+
+# Your existing deployment commands, e.g.:
+# Stop the existing service
+# Start the new service
+
 # Replace these variables with your actual values
 BOT_DIRECTORY="/home/ec2-user/ai-assistant"
 BOT_SCRIPT="discord_bot.py"
