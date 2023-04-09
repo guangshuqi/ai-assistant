d150d00eca1c0421c78f01fe318e9cf9ab616626
guangshuqi <guangshuqi@gmail.com>
2023-04-09 13:54:40 -0400

    got discord bot and llm working

---
+++ discord_bot.py
@@ -0,0 +1,51 @@
+import os
+import discord
+import openai
+from discord import Intents
+from discord.ext import commands
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv("DISCORD_BOT_TOKEN")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+intents = Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+openai.api_key = OPENAI_API_KEY
+conversation = [
+    {"role": "system", "content": "You are a helpful assistant."},
+]
+async def generate_response(messages):
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=messages
+    )
+    return response.choices[0].message["content"]
+
+@bot.event
+async def on_ready():
+    print(f"{bot.user} has connected to Discord!")
+
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+
+    # Add user message to conversation
+    conversation.append({"role": "user", "content": message.content})
+
+    response_text = await generate_response(conversation)
+    conversation.append({"role": "assistant", "content": response_text})
+
+    await message.channel.send(response_text)
+
+    # This line is important! It allows the bot to process commands.
+    await bot.process_commands(message)
+
+@bot.command(name="ping")
+async def ping(ctx):
+    await ctx.send("Pong!")
+
+bot.run(TOKEN)
