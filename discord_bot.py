4d03873dfcb118601f4e917ab41965c845a40137
John Qi <guangshuqi@gmail.com>
2023-04-11 22:41:27 -0400

    Update discord_bot.py
    Output openai api error msg to discord channel for debugging

--- discord_bot.py
+++ discord_bot.py
@@ -8,7 +8,7 @@ from discord.ext import commands
 from dotenv import load_dotenv
 from search_worker import search_worker
 
-search_queue = queue.Queue() 
+search_queue = queue.Queue()
 environment = os.environ.get("ENVIRONMENT")
 
 if environment == "production":
@@ -31,12 +31,15 @@ worker_thread = threading.Thread(target=search_worker, args=(search_queue, bot))
 worker_thread.start()
 
 async def generate_response(user_id, messages):
-    response = openai.ChatCompletion.create(
-        model="gpt-4",
-        # model="gpt-3.5-turbo",
-        messages=messages
-    )
-    return response.choices[0].message["content"]
+    try:
+        response = openai.ChatCompletion.create(
+            model="gpt-4",
+            # model="gpt-3.5-turbo",
+            messages=messages
+        )
+        return response.choices[0].message["content"], None
+    except Exception as e:
+        return None, str(e)
 
 @bot.event
 async def on_ready():
@@ -54,30 +57,33 @@ async def on_message(message):
     if user_id not in conversations:
         conversations[user_id] = [
             {
-                "role": "system", 
-                "content": (f"You are a helpful assistant. You are chatting with {user_name}."\
-                "First, provide information to the user. If you do not have needed information, "\
-                "search for this information by outputting the string `[Search Request]` followed by "\
-                "a concise description of needed information. You can also ask follow up questions to the user"\
-                "to clarify the information needed."
-                )
-                
+                "role": "system",
+                "content": (f"You are a helpful assistant. You are chatting with {user_name}."
+                            "First, provide information to the user. If you do not have needed information, "
+                            "search for this information by outputting the string `[Search Request]` followed by "
+                            "a concise description of needed information. You can also ask follow up questions to the user"
+                            "to clarify the information needed."
+                            )
+
             }
         ]
 
     # Add user message to conversation
     conversations[user_id].append({"role": "user", "content": message.content})
 
-    response_text = await generate_response(user_id, conversations[user_id])
-    conversations[user_id].append({"role": "assistant", "content": response_text})
+    response_text, error_message = await generate_response(user_id, conversations[user_id])
 
-
-    if response_text.startswith("[Search Request]"):
-        search_description = response_text[16:].strip()
-        search_queue.put((user_id, search_description, message.channel))
-        await message.channel.send(f"**_Searching for information:_** {search_description}")
+    if error_message:
+        await message.channel.send(f"**Error:** {error_message}")
     else:
-        await message.channel.send(response_text)
+        conversations[user_id].append({"role": "assistant", "content": response_text})
+
+        if response_text.startswith("[Search Request]"):
+            search_description = response_text[16:].strip()
+            search_queue.put((user_id, search_description, message.channel))
+            await message.channel.send(f"**_Searching for information:_** {search_description}")
+        else:
+            await message.channel.send(response_text)
 
     await bot.process_commands(message)
 
